e26ed0c67a4a91254529e468c5ced2db2430424e
ozay ozdemir <ozay.ozdemir@gmail.com>
2023-04-21 13:51:35 +0200

    new file:   .gitignore 	new file:   .npmrc 	new file:   README.md 	new file:   nuxt.config.ts 	new file:   package-lock.json 	new file:   package.json 	new file:   pages/about.vue 	new file:   pages/index.vue 	new file:   public/favicon.ico 	new file:   tsconfig.json

---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <p>this is about page</p>
+
+    <nuxt-link to="/">Go to Home</nuxt-link>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <nuxt-link to="/about">Go to about</nuxt-link>
+  </div>
+</template>
