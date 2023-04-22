e72dcbee972e7d016578487f9d6fd21a528e72db
ozay ozdemir <ozay.ozdemir@gmail.com>
2023-04-22 13:35:11 +0200

    Added directus 	Added directus 	Added directus

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts", "nuxt-icon"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-fonts",
+    "nuxt-icon",
+    "nuxt-directus",
+  ],
   googleFonts: {
     display: "swap",
     useStylesheet: true,
@@ -11,5 +16,8 @@ export default defineNuxtConfig({
       //"Open Sans": [300, 400, 500, 600, 700],
       //"Roboto Slab": [100, 200, 300, 400, 500, 600, 700, 800, 900],
     },
+  },
+  directus: {
+    url: "https://directus.ozayozdemir.com/",
   },
 });
