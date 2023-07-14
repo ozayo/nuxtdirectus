d1868941a6266c11a40fb367fe1075acdc62c549
ozay ozdemir <ozay.ozdemir@gmail.com>
2023-07-14 14:40:08 +0200

    edit homepage

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Welcome to the homepage</h1>
     <h2>Nuxt3 & Directus</h2>
+    <p>This is a test</p>
     <nuxt-link to="/about">Go to about</nuxt-link>
     <p>Icon test: <Icon name="uil:github" /></p>
   </div>
